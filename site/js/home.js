5a2d347fe941e4c11a2075f55436c66b728447d5
Shohag <learnwithmsbhh@gmail.com>
2022-07-22 07:28:41 +0600

    4

---
+++ site/js/home.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector("#toggle-btn")
+const toggleNavber = document.querySelector("#toggle-navber")
+
+toggleBtn.addEventListener("click",()=>{
+   toggleNavber.classList.toggle("add-toggle-nav")
+ 
+
+})
---
+++ site/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layouts/*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        "arima":["Arima"],
+        "ticket-name":["ticket-id"],
+        "cantate":["cantate"],
+        "edu":["Edu TAS Beginner"],
+        "nova":["Ibarra Real Nova"]
+      }
+    },
+
+  },
+  plugins: [],
+}
+//
